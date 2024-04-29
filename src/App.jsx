779fa6a3c2e124315ad341b4ae67bc1769ec7b51
Emre OZENSES <emreozenses@gmail.com>
2024-04-29 10:56:11 +0300

    alternative solutions added as note

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,40 @@ const [index,setIndex] = useState(0);
 const {image,job,text,name} = reviews[index];
 
 const handlePrev = ()=>{
-  setIndex(index-1);
-  if(index===0){
-    setIndex(reviews.length-1);
+  setIndex(index - 1)
+  if (index === 0) {
+    setIndex(reviews.length - 1)
   }
+
+  /* setIndex((currentIndex)=>{
+      const newIndex = (currentIndex-1+reviews.lenght)%reviews.length;
+      return newIndex;
+      }) */
+
+  /* setIndex((currentIndex)=>{
+      const newIndex = currentIndex+1;
+      if(newIndex<0){
+        return reviews.length-1;
+      }
+  }) */
 };
 const handleNext =()=>{
-  setIndex(index+1);
-  if(index===reviews.length-1){
-    setIndex(0);
+  setIndex(index + 1)
+  if (index === reviews.length - 1) {
+    setIndex(0)
   }
+
+  /* setIndex((currentIndex)=>{
+      const newIndex = (currentIndex+1)%reviews.length;
+      return newIndex;
+      }) */
+
+  /* setIndex((currentIndex)=>{
+      const newIndex = currentIndex+1;
+      if(newIndex>reviews.length-1){
+        return 0;
+      }
+  }) */
 };
 const handleRandom=()=>{
   const randomNumber = Math.round(Math.random()*(reviews.length-1));
@@ -30,22 +54,25 @@ const handleRandom=()=>{
 
   return (
     <main>
-      <div className="review">
+      <article className="review">
         <div className="img-container">
-                   
           <img src={image} className="person-img" />
-          <MdFormatQuote className="quote-icon" />
+          <span className="quote-icon">
+            <MdFormatQuote />
+          </span>
         </div>
-        <h3 className="author">{name}</h3>
-        <h8 className="job">{job}</h8>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
         <p className="info">{text}</p>
-        
-        <div className="btn-container">
-          <BiChevronLeft className="prev-btn" onClick={handlePrev}/>
-          <BiChevronRight className="next-btn" onClick={handleNext}/>
-        </div>
-        <button type="button" onClick={handleRandom} className="btn">Surprise Me</button>
+      <div className="btn-container">
+        <BiChevronLeft className="prev-btn" onClick={handlePrev} />
+        <BiChevronRight className="next-btn" onClick={handleNext} />
       </div>
+      <button type="button" onClick={handleRandom} className="btn btn-hipster">
+        Surprise Me
+      </button>
+      </article>
+
     </main>
   )};
 export default App;
