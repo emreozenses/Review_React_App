5967aaca5a85e4d61b5dd23ee6751d2b0c06c0ad
Emre OZENSES <emreozenses@gmail.com>
2024-04-29 10:03:34 +0300

    random button added

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,44 @@ import { MdFormatQuote } from 'react-icons/md';
 
 const App = () => {
 
-const [review,setReview] = useState(reviews[0]);
-const handlePrev = ()=>{};
-const handleNext =()=>{
+const [index,setIndex] = useState(0);
+const {image,job,text,name} = reviews[index];
+
+const handlePrev = ()=>{
+  setIndex(index-1);
+  if(index===0){
+    setIndex(reviews.length-1);
+  }
 };
+const handleNext =()=>{
+  setIndex(index+1);
+  if(index===reviews.length-1){
+    setIndex(0);
+  }
+};
+const handleRandom=()=>{
+  const randomNumber = Math.round(Math.random()*(reviews.length-1));
+    setIndex(randomNumber);
+   console.log(randomNumber);
+}
 
   return (
     <main>
       <div className="review">
         <div className="img-container">
                    
-          <img src={review.image} className="person-img" />
+          <img src={image} className="person-img" />
           <MdFormatQuote className="quote-icon" />
         </div>
-        <h3 className="author">{review.name}</h3>
-        <h8 className="job">{review.job}</h8>
-        <p className="info">{review.text}</p>
+        <h3 className="author">{name}</h3>
+        <h8 className="job">{job}</h8>
+        <p className="info">{text}</p>
         
         <div className="btn-container">
-          <BiChevronLeft className="prev-btn" onClick={handlePrev()}/>
-          <BiChevronRight className="next-btn" onClick={handleNext()}/>
+          <BiChevronLeft className="prev-btn" onClick={handlePrev}/>
+          <BiChevronRight className="next-btn" onClick={handleNext}/>
         </div>
+        <button type="button" onClick={handleRandom} className="btn">Surprise Me</button>
       </div>
     </main>
   )};
